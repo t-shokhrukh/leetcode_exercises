697b420e42790e5e724ae7de58181f62a8fbb799
w0xrux <dev.w0xrux@gmail.com>
2024-05-24 17:30:33 +0500

    add first ans

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::mem::forget;
+
+fn main() {}
+
+fn two_sum_first_ans(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    let mut res: Vec<i32> = Vec::new();
+    for i in 0..nums.len() {
+        for j in (i + 1)..nums.len() {
+            if nums[i] + nums[j] == target {
+                res.push(i as i32);
+                res.push(j as i32);
+            }
+        }
+    }
+    return res;
 }
