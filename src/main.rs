abc5efdb59d2a6b0218d88d11daa21f714090d0e
w0xrux <dev.w0xrux@gmail.com>
2024-05-27 18:48:19 +0500

    score of string

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ use std::collections::HashMap;
 fn main() {}
 
 fn two_sum_first_ans(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    /* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-     You may assume that each input would have exactly one solution, and you may not use the same element twice.*/
     let mut res: Vec<i32> = Vec::new();
     for i in 0..nums.len() {
         for j in (i + 1)..nums.len() {
@@ -14,26 +12,15 @@ fn two_sum_first_ans(nums: Vec<i32>, target: i32) -> Vec<i32> {
             }
         }
     }
-    return res;
+    res
 }
 
-fn is_palindrome_num_first_ans(x: i32) -> bool {
-    // Given an integer x, return true if x is a palindrome, and false otherwise.
-    let reversed: String = x.to_string().chars().rev().collect();
-    x.to_string() == reversed
+fn is_palindrome_num_first_ans(n: i32) -> bool {
+    let reversed: String = n.to_string().chars().rev().collect();
+    n.to_string() == reversed
 }
 
 fn roman_to_int_first_ans(s: String) -> i32 {
-    /*  Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
-        Given a roman numeral, convert it to an integer.
-         I             1
-         V             5
-         X             10
-         L             50
-         C             100
-         D             500
-         M             1000
-     */
     let mut symbols: HashMap<char, i32> = HashMap::new();
     symbols.insert('I', 1);
     symbols.insert('V', 5);
@@ -56,5 +43,14 @@ fn roman_to_int_first_ans(s: String) -> i32 {
             prev_value = value;
         }
     }
-    return res;
+    res
+}
+
+fn score_of_string(s: String) -> i32 {
+    let mut score: i32 = 0;
+    let bytes: &[u8] = s.as_bytes();
+    for i in 0..bytes.len() - 1 {
+        score += (bytes[i] as i32 - bytes[i + 1] as i32).abs();
+    }
+    score
 }
