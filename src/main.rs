fcc6a74ac6c6d5bdb8264a66041f6cfe59438989
w0xrux <dev.w0xrux@gmail.com>
2024-05-28 15:56:15 +0500

    fibonacci

--- src/main.rs
+++ src/main.rs
@@ -54,3 +54,13 @@ fn score_of_string(s: String) -> i32 {
     }
     score
 }
+
+fn fibonacci(n: i32) -> i32 {
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
+    fibonacci(n - 1) + fibonacci(n - 2)
+}
